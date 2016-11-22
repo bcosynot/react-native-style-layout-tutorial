f449a12bfe16ac8d0c4a5608e15b1ca93db76fee
Vivek Ranjan <vr3690@gmail.com>
2016-11-21 22:22:42 -0600

    Showing off flex

--- index.android.js
+++ index.android.js
@@ -15,18 +15,28 @@ import {
 export default class expertgroup extends Component {
     render() {
         return (
-            <View style={styles.welcomeMsgContainer}>
-                <Text style={styles.welcomeMsg}>
-                    Hello World!
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.welcomeMsgContainer}>
+                    <Text style={styles.welcomeMsg}>
+                        Hello World!
+                    </Text>
+                </View>
+                <View style={styles.jumbotronContainer}>
+                    <Text style={styles.jumbotron}>
+                        Welcome to react native
+                    </Text>
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {flexDirection: 'column', alignItems: 'flex-end'},
     welcomeMsgContainer: {backgroundColor: '#ff0000'},
-    welcomeMsg: {color: '#fff', fontWeight: 'bold'}
+    welcomeMsg: {color: '#fff', fontWeight: 'bold'},
+    jumbotronContainer: {backgroundColor: '#ffff00'},
+    jumbotron: {color: '#000'}
 });
 
 
